0907f4006cf64c4a2eb4832feebd97463cc32391
David <jesusdavidburgosrubio@gmail.com>
2026-05-22 18:39:34 -0500

    Ofuscación del javascript.

--- frontend-web/vite.config.js
+++ frontend-web/vite.config.js
@@ -1,10 +1,29 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
+import obfuscator from "vite-plugin-javascript-obfuscator";
 
-export default defineConfig({
-  plugins: [react(), tailwindcss()],
+export default defineConfig(({ command }) => ({
+  plugins: [
+    react(),
+    tailwindcss(),
+
+    command === "build" &&
+      obfuscator({
+        options: {
+          compact: true,
+          controlFlowFlattening: true,
+          deadCodeInjection: false,
+          stringArray: true,
+          stringArrayThreshold: 0.75,
+          debugProtection: true,
+          disableConsoleOutput: true,
+          identifierNamesGenerator: "hexadecimal",
+        },
+      }),
+  ].filter(Boolean),
+
   server: {
     port: 5173,
   },
-});
+}));
